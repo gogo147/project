72b28819b1fb467e176a4a952d97b0a807851cbe
George Manaz <george@MacBook-Air-som-tillhor-George.local>
2023-10-18 20:18:48 +0200

    new update

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer>
+      <div className='row'>
+        <div className='col'>
+          <p>
+            Lorem ipsum is placeholder text commonly 
+            used in the graphic, print, and publishing 
+            industries for previewing layouts and 
+            visual mockups
+          </p>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
